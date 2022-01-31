ba46a93e8506b7d70b2035db1ea1d945b161c427
Jv Borges <borges.jvdo@gmail.com>
2022-01-31 14:24:04 -0300

    Testando env novamente

--- services/firebase.ts
+++ services/firebase.ts
@@ -6,11 +6,11 @@ import { getFirestore, collection } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const apiKey = process.env.NEXT_APIKEY;
-const authDomain = process.env.NEXT_AUTHDOMAIN;
-const projectId = process.env.NEXT_PROJECTID;
-const storageBucket = process.env.NEXT_STORAGE_BUCKET;
-const messagingSenderId = process.env.NEXT_MESSAGING_SENDER_ID;
+const apiKey = process.env.NEXT_PUBLIC_APIKEY;
+const authDomain = process.env.NEXT_PUBLIC_AUTHDOMAIN;
+const projectId = process.env.NEXT_PUBLIC_PROJECTID;
+const storageBucket = process.env.NEXT_PUBLIC_STORAGE_BUCKET;
+const messagingSenderId = process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID;
 const appId = process.env.NEXT_APP_ID;
 
 const firebaseConfig = {
@@ -22,17 +22,8 @@ const firebaseConfig = {
   appId,
 };
 
-const firebaseConfig2 = {
-  apiKey: "AIzaSyB4qjjWepX-4SN8a2NH4TY-EhOZcr3K2Ac",
-  authDomain: "agenda-barber.firebaseapp.com",
-  projectId: "agenda-barber",
-  storageBucket: "agenda-barber.appspot.com",
-  messagingSenderId: "902105140343",
-  appId: "1:902105140343:web:67cf8e13fad8b547c24a72",
-};
-
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig2);
+const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
 
 export { db };
