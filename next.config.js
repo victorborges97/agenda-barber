be0150434bab482de6bbc359c4e6011fac1e0387
Jv Borges <borges.jvdo@gmail.com>
2022-01-31 12:43:33 -0300

    Variaveis de ambiente nao funcionou

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_APIKEY: process.env.NEXT_APIKEY,
+    NEXT_AUTHDOMAIN: process.env.NEXT_AUTHDOMAIN,
+    NEXT_PROJECTID: process.env.NEXT_PROJECTID,
+    NEXT_STORAGE_BUCKET: process.env.NEXT_STORAGE_BUCKET,
+    NEXT_MESSAGING_SENDER_ID: process.env.NEXT_MESSAGING_SENDER_ID,
+    NEXT_APP_ID: process.env.NEXT_APP_ID,
+  }
 }
 
 module.exports = nextConfig
--- services/firebase.ts
+++ services/firebase.ts
@@ -1,21 +1,38 @@
 // Import the functions you need from the SDKs you need
+import firebase from "firebase/compat/app";
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+const apiKey = process.env.NEXT_APIKEY;
+const authDomain = process.env.NEXT_AUTHDOMAIN;
+const projectId = process.env.NEXT_PROJECTID;
+const storageBucket = process.env.NEXT_STORAGE_BUCKET;
+const messagingSenderId = process.env.NEXT_MESSAGING_SENDER_ID;
+const appId = process.env.NEXT_APP_ID;
+
 const firebaseConfig = {
-  apiKey: process.env.NEXT_APIKEY,
-  authDomain: process.env.NEXT_AUTHDOMAIN,
-  projectId: process.env.NEXT_PROJECTID,
-  storageBucket: process.env.NEXT_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_APP_ID,
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+};
+
+const firebaseConfig2 = {
+  apiKey: "AIzaSyB4qjjWepX-4SN8a2NH4TY-EhOZcr3K2Ac",
+  authDomain: "agenda-barber.firebaseapp.com",
+  projectId: "agenda-barber",
+  storageBucket: "agenda-barber.appspot.com",
+  messagingSenderId: "902105140343",
+  appId: "1:902105140343:web:67cf8e13fad8b547c24a72",
 };
 
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig2);
 const db = getFirestore(firebaseApp);
 
 export { db };
